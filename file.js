44b6dcce47d616a5bfb31cdbceb989fde40981e7
Noorlogs <125485692+Noorlogs@users.noreply.github.com>
2023-04-07 20:37:01 +0500

    zakaat value alert

---
+++ file.js
@@ -0,0 +1,9 @@
+let  zakaatPer = 0.025;
+let userInput;
+userInput =  Number(prompt('Enter value to know your amount of zakaat'));
+let result = zakaatPer * userInput;
+
+ 
+// alert('your zakaat value is ${result}');
+alert("your Zakaat Value is "+result);
+// document.write('your Zakaat Value is '+result);
